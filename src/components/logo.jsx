fefd117869f9838929d912cba9fb4aaec18d482f
xumemgling <18838983960@163.com>
2018-03-13 16:50:32 +0800

    init

---
+++ src/components/logo.jsx
@@ -0,0 +1,12 @@
+import  React from 'react'
+import logoImg from './job.png'
+import './logo.less'
+class Logo extends React.Component{
+    render(){
+        return(
+            <div className="logo-container">
+            <img src={logoImg} alt="logo"/>
+            </div>)
+    }
+}
+export default Logo
--- src/containers/register/register.jsx
+++ src/containers/register/register.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React,{Component} from 'react'
+import {NavBar,WingBlank,List,InputItem,WhiteSpace,Radio,Button} from 'antd-mobile'
+
+import Logo from '../../components/logo'
+const RadioItem=Radio.RadioItem;
 export default class Register extends React.Component{
+        goLogin =()=>{
+            this.props.history.replace('/login')
+        }
+        //注册收集数据
+    handleRegister=()=>{}
     render(){
         return (
-            <div>Register</div>
+            <div><NavBar>硅谷直聘</NavBar>
+                <Logo/>
+                <WingBlank>
+                    <List>
+                        <InputItem>用户名:</InputItem>
+                        <WhiteSpace/>
+                        <InputItem type="password">密码:</InputItem>
+                        <WhiteSpace/>
+                        <InputItem type="password">确认密码:</InputItem>
+                        <WhiteSpace/>
+                        <InputItem checked>BOSS:</InputItem>
+                        <WhiteSpace/>
+                        <InputItem >牛人:</InputItem>
+                        <Button type="primary" onClick={this.handleRegister}>注册</Button>
+                        <WhiteSpace/>
+                        <Button onClick={this.goLogin}>已经有账号</Button>
+                    </List>
+                </WingBlank>
+
+            </div>
         )
 
     }
